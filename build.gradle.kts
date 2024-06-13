5b787943d887a8971f407915e939db4c8c35c7ac
Kiko13bb <kiko13bb@gmail.com>
2024-06-13 21:54:02 +0200

    Enable Firebase's crashlytics and performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.1" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
