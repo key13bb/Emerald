a5b5d843826467728b59870e93816017b2859e4a
Kiko13bb <kiko13bb@gmail.com>
2024-06-13 06:44:38 +0200

    Deleted unused imports

--- app/src/main/java/star/kiko13bb/emerald/composable/NavigationBar.kt
+++ app/src/main/java/star/kiko13bb/emerald/composable/NavigationBar.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationRail
