ba2f7c4118c5cde4b280d7c4d9e3a51d1a76d9e2
Kiko13bb <kiko13bb@gmail.com>
2024-08-09 23:13:51 +0200

    Update to 0.0.5 Refactored most of screens Added transports icons Working firebase settings New dialog template New Download page New Transport page Optimized code and size Starting to move to Proto DataStore Updated libraries Support for new signature versions (v1-v4)

--- app/src/main/java/star/kiko13bb/emerald/ui/straight/CompactSettingsScreen.kt
+++ app/src/main/java/star/kiko13bb/emerald/ui/straight/CompactSettingsScreen.kt
@@ -1,5 +1,6 @@
 package star.kiko13bb.emerald.ui.straight
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import star.kiko13bb.emerald.ui.components.CommonDialog
 import star.kiko13bb.emerald.ui.components.SettingItem
 import star.kiko13bb.emerald.ui.components.dialogs
@@ -23,7 +25,8 @@ val settingsFlow: Flow<Boolean> = MainActivity().settingsDataStore.data.map { se
 
 @Composable
 fun CompactSettingsScreen() {
-    Column {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)) {
         titles.forEachIndexed { index, _ ->
             Row(
                 modifier = Modifier.fillMaxWidth(),
