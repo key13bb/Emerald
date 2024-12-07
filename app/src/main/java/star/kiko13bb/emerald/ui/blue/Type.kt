5ea326c58c41ec3880c9a8e41aca73200cd5f709
Francisco Bravo Bravo <key13bb@gmail.com>
2024-12-07 02:30:25 +0100

    Delete unused resources except transport types. Implemented bottom navigation bar and navigation rail (synced) without any activity. Add support for Android 13's per app language. Add monochrome icon support Add splash screen animation (Android 12+) Add more details to commit message Update to 0.0.1

---
+++ app/src/main/java/star/kiko13bb/emerald/ui/blue/Type.kt
@@ -0,0 +1,5 @@
+package star.kiko13bb.emerald.ui.blue
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
---
+++ app/src/main/java/star/kiko13bb/emerald/ui/yellow/Type.kt
@@ -0,0 +1,5 @@
+package star.kiko13bb.emerald.ui.yellow
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
