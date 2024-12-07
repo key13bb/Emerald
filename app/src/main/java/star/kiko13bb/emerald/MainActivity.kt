afd1f5034b8fa2939a9aff717c0424d39fd03981
Francisco Bravo Bravo <key13bb@gmail.com>
2024-12-07 02:29:41 +0100

    Create metro cards and animated splashScreen

--- app/src/main/java/star/kiko13bb/emerald/MainActivity.kt
+++ app/src/main/java/star/kiko13bb/emerald/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,9 +20,13 @@ import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import star.kiko13bb.emerald.ui.theme.EmeraldTheme
+import star.kiko13bb.emerald.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
@@ -36,7 +43,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen (modifier: Modifier = Modifier) {
-    //TODO: Develop Main Screens//
+    MetroCards()
 }
 
 @Composable
@@ -137,5 +144,74 @@ fun EmeraldApp(windowSize: WindowSizeClass) {
         WindowWidthSizeClass.Expanded -> {
             MainLandscape()
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier, colors: Color) {
+    Surface(
+        color = colors,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        shape = MaterialTheme.shapes.extraLarge,
+    ) {
+        Row(modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(12.dp)
+            ) {
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { /* TODO */ }
+            ) {
+                Text("Ver línea", color = Color(colors.value))
+            }
+        }
+    }
+}
+
+@Composable
+fun MetroCards(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("Línea 1", "Línea 2"),
+    colors: List<Color> = listOf(metro_l1, metro_l2_cerca_c1)
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name, colors = colors[names.indexOf(name)])
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardPreview() {
+    EmeraldTheme {
+        Greeting("Línea 1", modifier = Modifier.padding(8.dp), colors = metro_l1)
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 640)
+@Composable
+fun PortraitPreview() {
+    EmeraldTheme {
+        MainPortrait()
+    }
+}
+
+@Preview(widthDp = 640, heightDp = 360)
+@Composable
+fun LandscapePreview() {
+    EmeraldTheme {
+        MainLandscape()
     }
 }
